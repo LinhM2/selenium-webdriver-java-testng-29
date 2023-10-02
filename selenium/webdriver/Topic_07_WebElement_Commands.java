aa685227bc09f11f1c2da1c0ad7afe3a8d80ff19
LinhM2 <linhmit9@gmail.com>
2023-10-02 17:15:33 +0700

    topic 21 2/3

--- selenium/webdriver/Topic_07_WebElement_Commands.java
+++ selenium/webdriver/Topic_07_WebElement_Commands.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Topic_07_WebElement_Commands {
     WebDriver driver;
@@ -23,6 +27,7 @@ public class Topic_07_WebElement_Commands {
 
     @Test
     public void TC_01_Element() {
+        /*// (1)
         // html element: Textbox/ testarea/ link/ button/...
         // Tìm và trả về 1 element
         driver.findElement(By.id(""));
@@ -36,8 +41,57 @@ public class Topic_07_WebElement_Commands {
         WebElement fullNameTextbox = driver.findElement(By.id(""));
         fullNameTextbox.clear();
         fullNameTextbox.sendKeys("test abc");
+        fullNameTextbox.getAttribute("value");*/
+
+        //(2) các hàm
+        // Dùng để xóa dữ liệu trong 1 field cho phép nhập (editable)
+        // Textbox/ testarea/ dropdown (editable)
+        // thường được sủ dụng trước hàm sendkeys
+        driver.findElements(By.id("")).clear();
+
+
+        // Dùng để nhập liệu vào các field bên trên
+        driver.findElement(By.id("")).sendKeys("");
+
+        // Dùng để click lên Element
+        driver.findElement(By.id("")).click();
+
+
+        // Tìm từ nốt cha vào nốt con
+        driver.findElement(By.id("form-validate")).findElement(By.id("firsname"));
+        driver.findElement(By.cssSelector("form#form-validate input#firsname"));
+
+        // Trả về 1 element khớp với điều kiện
+        WebElement fullNameTextbox = driver.findElement(By.id(""));
+
+        // Tra về nhiều element khớp với điểu kiện
+        List<WebElement> testboxes = driver.findElements(By.cssSelector(""));
+
+        // DÙng để verify 1 checkbox/ radio/ dropdown đã được chọn hay chưa
+        Assert.assertTrue(driver.findElement(By.id("")).isSelected());
+        Assert.assertFalse(driver.findElement(By.id("")).isSelected());
+
+        // Dropdown (default/ custom)
+        Select select = new Select(driver.findElement(By.id("")));
+
+        // Dùng để verify 1 element bất kỳ có hiển thị hay không
+        Assert.assertTrue(driver.findElement(By.id("")).isDisplayed());
+        Assert.assertFalse(driver.findElement(By.id("")).isDisplayed());
+
+        // Dùng để verify 1 element có được thao tác lên hay không (không phải read-only)
+        Assert.assertFalse(driver.findElement(By.id("")).isEnabled());
+
+        // HTML Element
+        // <input type="text" id="firstname" name="firstname" value=""
+        // title="First Name" maxlength="255" class="input-text required-entry">
+        driver.findElement(By.id("")).getAttribute("title");
+        driver.findElement(By.id("")).getAttribute("name");
 
         //
+        driver.findElement(By.id("")).getAccessibleName();
+        driver.findElement(By.id("")).getDomAttribute("checked");
+        driver.findElement(By.id("")).getDomProperty("");
+
 
 
     }
