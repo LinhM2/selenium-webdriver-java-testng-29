64bfe7d7f9131c4a7bc0695b77a03eb0a058b687
LinhM2 <linhmit9@gmail.com>
2023-10-14 23:00:20 +0700

    default dropdown 1

---
+++ selenium/javaTester/Topic_01_Data_type.java
@@ -0,0 +1,20 @@
+package javaTester;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+public class Topic_01_Data_type {
+    //Kieu du lieu trong Java
+    // I - Kiểu dữ liệu nguyên thủy (Primitive Type)
+    // Số nguyên: byte - short - int - long
+    // Số thực: float - double
+    // Ký tu: char
+    // Logic: boolean
+    // II - Kiểu dữ liệu tham chiếu (Reference Type)
+    // String - Chuỗi ký tự
+    // Class
+    // Object
+    // Array
+    // Collection: List/ Set/ Queue
+    JavascriptExecutor javascriptExecutor;
+
+}
---
+++ selenium/webdriver/Topic_09_Default_Dropdown.java
@@ -0,0 +1,109 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Random;
+
+public class Topic_09_Default_Dropdown {
+    WebDriver driver;
+    String firstName = "abc", lastName = "TKH", emailAddress = getEmailAddress();
+    String companyName = "Selenium webDriver", password = "123456";
+    String day = "15", month = "May", year = "1950";
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+    }
+
+    @Test
+    public void TC_01_Register() {
+        driver.findElement(By.className("a.ico-register")).click();
+
+        driver.findElement(By.id("FirstName")).sendKeys(firstName);
+        driver.findElement(By.id("LastName")).sendKeys(lastName);
+
+        // dayDropdown dropdown
+        Select dayDropdown = new Select(driver.findElement(By.name("DateOfBirthDay")));
+
+        // Chọn ngày
+        dayDropdown.selectByVisibleText(day);
+
+        // Verify dropdown này là single (k phải multiple)
+        Assert.assertFalse(dayDropdown.isMultiple());
+
+        // Verify số lượng item trong Dropdown này là 32 item
+        Assert.assertEquals(dayDropdown.getOptions().size(),32);
+
+        new Select(driver.findElement(By.name("DateOfBirthMonth"))).selectByVisibleText(month);
+        new Select(driver.findElement(By.name("DateOfBirthYear"))).selectByVisibleText(year);
+
+        driver.findElement(By.id("Company")).sendKeys(companyName);
+        driver.findElement(By.id("Email")).sendKeys(emailAddress);
+        driver.findElement(By.id("Password")).sendKeys(password);
+        driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+
+        driver.findElement(By.id("register-button")).click();
+        sleepINSeconds(3);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.result")).getText(),"Your registration completed");
+
+    }
+
+    @Test
+    public void TC_02_Login() {
+        driver.get("https://demo.nopcommerce.com/");
+
+        // Login
+        driver.findElement(By.cssSelector("a.ico-login")).click();
+        driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAddress);
+        driver.findElement(By.cssSelector("input#Password")).sendKeys(password);
+        driver.findElement(By.cssSelector("button.login-button")).click();
+        sleepINSeconds(3);
+
+        // Verify
+        driver.findElement(By.cssSelector("a.ico-account")).click();
+        sleepINSeconds(2);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#firstname")).getAttribute("value"),firstName);
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#lastname")).getAttribute("value"),lastName);
+
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthDay"))).getFirstSelectedOption().getText(), day);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthMonth"))).getFirstSelectedOption().getText(), month);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthYear"))).getFirstSelectedOption().getText(), year);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#email")).getAttribute("value"),emailAddress);
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#Company")).getAttribute("value"),companyName);
+
+    }
+
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    public void sleepINSeconds(long timeINSecond) {
+        try {
+            Thread.sleep(timeINSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getEmailAddress() {
+        Random rand = new Random();
+        return "abcTKH" + rand.nextInt(99999) + "@gmail.net";
+    }
+
+}
