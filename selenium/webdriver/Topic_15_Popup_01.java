a898fb8cd4433039b037fe65726a5a6b3ef226c2
LinhM2 <linhmit9@gmail.com>
2023-12-29 23:51:13 +0700

    popup 1

--- selenium/webdriver/Topic_15_Popup_01.java
+++ selenium/webdriver/Topic_15_Popup_01.java
@@ -22,6 +22,7 @@ public class Topic_15_Popup_01 {
 
     @Test
     public void TC_01_Fixed_Popup_In_DOM_01() {
+        // TC01_Topic 11/12
         driver.get("https://ngoaingu24h.vn/");
 
         driver.findElement(By.cssSelector("button.login_")).click();
@@ -51,17 +52,78 @@ public class Topic_15_Popup_01 {
 
     @Test
     public void TC_02_Fixed_Popup_In_DOM_02() {
+        // TC02_Topic 11/12
         driver.get("https://skills.kynaenglish.vn/");
 
         driver.findElement(By.cssSelector("a.login-btn")).click();
         sleepINSeconds(2);
+        // Kiểm tra hiển thị popup
+        Assert.assertTrue(driver.findElement(By.cssSelector("div#k-popup-account-login-mb div.modal-content")).isDisplayed());
 
+        // Login
+        driver.findElement(By.cssSelector("input#user-login")).sendKeys("test@gmail.com");
+        driver.findElement(By.cssSelector("input#user-password")).sendKeys("123456");
+        driver.findElement(By.cssSelector("button#btn-submit-login")).click();
+        sleepINSeconds(2);
+
+        // vetify
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.password-form-login-message")).getText(),"Sai tên đăng nhập hoặc mật khẩu");
+
+        // đóng popup
+        driver.findElement(By.cssSelector("button.k-popup-account-close")).click();
+
+        // Kiểm tra hiển k còn thị popup
+        Assert.assertFalse(driver.findElement(By.cssSelector("div#k-popup-account-login-mb div.modal-content")).isDisplayed());
 
 
     }
 
     @Test
-    public void TC_03_() {
+    public void TC_03_Fixed_Popup_Not_In_DOM_01() {
+        // TC03_Topic 11/12
+        driver.get("https://tiki.vn/");
+
+        // Mở popup
+        driver.findElement(By.cssSelector("div[data-view-id='header_header_account_container']")).click();
+        sleepINSeconds(2);
+
+        // Kiểm tra popup hiển thị
+        Assert.assertTrue(driver.findElement(By.cssSelector("div.ReactModal__Overlay")).isDisplayed());
+
+        driver.findElement(By.cssSelector("p.login-with-email")).click();
+        sleepINSeconds(2);
+
+        // không nhập gì -> login
+        driver.findElement(By.cssSelector("input[type='email']")).clear();
+        driver.findElement(By.cssSelector("input[type='password']")).clear();
+        driver.findElement(By.xpath("//button[text()='Đăng nhập']")).click();
+        sleepINSeconds(2);
+
+        // verify msg hiển thị
+        Assert.assertEquals(driver.findElement(By.xpath("//input[@type='email']/parent::div/following-sibling::span[1]")).getText(),"Email không được để trống");
+        Assert.assertEquals(driver.findElement(By.xpath("//input[@type='email']/parent::div/following-sibling::span[2]")).getText(),"Mật khẩu không được để trống");
+
+        // đóng popup
+        driver.findElement(By.cssSelector("button.btn-close")).click();
+        sleepINSeconds(2);
+
+        // Kiểm tra popup không còn hiển thị
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Assert.assertEquals(driver.findElements(By.cssSelector("div.ReactModal__Overlay")).size(),0);
+
+    }
+
+    @Test
+    public void TC_04_Fixed_Popup_Not_In_DOM_02() {
+        // TC04_Topic 11/12
+        driver.get("https://www.facebook.com/");
+
+        // Mở popup
+        driver.findElement(By.cssSelector("a[data-testid='open-registration-form-button']")).click();
+        sleepINSeconds(2);
+
+       // Assert.assertTrue(driver.findElement(By.cssSelector("")));
+
 
     }
 
---
+++ selenium/webdriver/Topic_22_Wait_01_Element_Status.java
@@ -0,0 +1,135 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_22_Wait_01_Element_Status {
+    WebDriver driver;
+    WebDriverWait explicitWait;
+    By reconfirmEmailTextbox = By.cssSelector("input[name='reg_email_confirmation__']");
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        //driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_01_Visible() {
+        driver.findElement(By.cssSelector("a[data-testid='open-registration-form-button']")).click();
+        sleepINSeconds(2);
+
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).sendKeys("linh@gmail.com");
+        sleepINSeconds(3);
+
+        // điều kiện 1: element có xuất hiện ở trên UI và có trong cây html
+        // Tại đúng thời điểm này thì confirm email textbox đang visibe/displayed (hiển thị)
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(reconfirmEmailTextbox));
+
+        // Kiểm tra 1 element đang hiển thị
+        Assert.assertTrue(driver.findElement(reconfirmEmailTextbox).isDisplayed());
+
+    }
+
+    @Test
+    public void TC_02_Invisible_IN_DOM() {
+        driver.findElement(By.cssSelector("a[data-testid='open-registration-form-button']")).click();
+        sleepINSeconds(2);
+
+        // điều kiện 2: element k xuất hiện trên UI và vẫn có trong cây html
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).clear();
+        sleepINSeconds(3);
+
+        // Tại đúng thời điểm này/step này thì confirm email textbox đang invisible
+        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(reconfirmEmailTextbox));
+
+        // Kiểm tra 1 element không hiển thị
+        // chạy nhanh -> kết quả step này passed
+        Assert.assertFalse(driver.findElement(reconfirmEmailTextbox).isDisplayed());
+
+    }
+    @Test
+    public void TC_02_Invisible_NOT_IN_DOM() {
+        driver.findElement(By.cssSelector("a[data-testid='open-registration-form-button']")).click();
+        sleepINSeconds(2);
+
+        // Click vào icon close để đóng popup
+        driver.findElement(By.xpath("//div[text()='Sign Up']/parent::div/preceding-sibling::img")).click();
+        sleepINSeconds(3);
+
+        // điều kiện 3: element k xuất hiện trên UI và cũng k có trong cây html
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).clear();
+        sleepINSeconds(3);
+
+        // Tại đúng thời điểm này/step này thì confirm email textbox đang invisible
+        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(reconfirmEmailTextbox));
+
+        // Kiểm tra 1 element không hiển thị
+        // chạy lâu -> kết quả step này failed
+        // Assert.assertFalse(driver.findElement(reconfirmEmailTextbox).isDisplayed());
+        // -> vậy verify ntn???
+    }
+
+    @Test
+    public void TC_03_Presence() {
+        driver.findElement(By.cssSelector("a[data-testid='open-registration-form-button']")).click();
+        sleepINSeconds(2);
+
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).sendKeys("linh@gmail.com");
+        sleepINSeconds(3);
+
+        // điều kiện 1: element có xuất hiện ở trên UI và có trong cây html
+        // Tại đúng thời điểm này thì confirm email textbox presence (có trong html)
+        explicitWait.until(ExpectedConditions.presenceOfElementLocated(reconfirmEmailTextbox));
+
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).clear();
+        sleepINSeconds(3);
+
+        // điều kiện 2: element k xuất hiện trên UI và vẫn có trong cây html
+        explicitWait.until(ExpectedConditions.presenceOfElementLocated(reconfirmEmailTextbox));
+
+    }
+
+    @Test
+    public void TC_04_Staleness() {
+        driver.findElement(By.cssSelector("a[data-testid='open-registration-form-button']")).click();
+        sleepINSeconds(2);
+
+        // Tại thời điểm này element có xuất hiện và mình sẽ findelement
+        WebElement reconfirmEmail = driver.findElement(reconfirmEmailTextbox);
+
+        // Click vào icon close để đóng popup
+        driver.findElement(By.xpath("//div[text()='Sign Up']/parent::div/preceding-sibling::img")).click();
+        sleepINSeconds(3);
+
+        // điều kiện 3: element k xuất hiện trên UI và cũng k có trong cây html
+        explicitWait.until(ExpectedConditions.stalenessOf(reconfirmEmail));
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+    public void sleepINSeconds(long timeINSecond) {
+        try {
+            Thread.sleep(timeINSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
