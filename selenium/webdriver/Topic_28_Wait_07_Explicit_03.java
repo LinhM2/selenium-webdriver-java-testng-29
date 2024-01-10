7379d5ffcd0ac8820d55490d3ab6fd65efbb0717
LinhM2 <linhmit9@gmail.com>
2024-01-10 22:42:26 +0700

    Wait - Part VI

---
+++ selenium/webdriver/Topic_28_Wait_07_Explicit_03.java
@@ -0,0 +1,68 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_28_Wait_07_Explicit_03 {
+    WebDriver driver;
+    WebDriverWait explicitWait;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        explicitWait = new WebDriverWait(driver,Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void TC_01_Ajaxloading() {
+        // Topic 15/16: TC 06
+        driver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
+
+        // dùng 2 lần nên sẽ đặt biến (đặt biến By, k đặt biến driver sẽ bị lỗi)
+        By selectedDateBy = By.cssSelector("span#ctl00_ContentPlaceholder1_Label1");
+
+        Assert.assertEquals(driver.findElement(selectedDateBy).getText(),"No Selected Dates to display.");
+
+        driver.findElement(By.xpath("//a[text()='12']")).click();
+
+        // wait cho loading icon biến mất
+        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div[id*='RadCalendar1']>div.raDiv")));
+
+        Assert.assertEquals(driver.findElement(selectedDateBy).getText(),"Friday, January 12, 2024");
+    }
+
+    @Test
+    public void TC_02_Upload_File() {
+        // Topic 15/16: TC 06
+        driver.get("https://gofile.io/welcome");
+
+        // Wait + verify spinner icon biến mất
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.spinner-border"))));
+
+        // Wait + Click
+        explicitWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.spinner-border"))).click();
+
+        // Wait + verify spinner icon biến mất
+        Assert.assertTrue(explicitWait.until(ExpectedConditions.invisibilityOfAllElements(driver.findElements(By.cssSelector("div.spinner-border")))));
+
+        driver.findElement(By.cssSelector("input[type='file']")).sendKeys();
+        // 1h20 -
+
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
---
+++ selenium/webdriver/Topic_29_Wait_08_Mix_Implicit_Explicit.java
@@ -0,0 +1,92 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_29_Wait_08_Mix_Implicit_Explicit {
+    WebDriver driver;
+    WebDriverWait explicitWait;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void TC_01_Only_Implicit_Found() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://www.facebook.com/");
+
+        // khi vào tìm element thì nó tìm thấy ngay
+        // k cần chờ hết timeout
+        driver.findElement(By.cssSelector("input#email"));
+    }
+
+    @Test
+    public void TC_02_Only_Implicit_Not_Found() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://www.facebook.com/");
+
+        // khi vào tìm element không tìm thấy
+        // polling mỗi nửa s tìm lại 1 lần
+        // khi hết timeout sẽ đánh fail testcase và throw exception: NoSuchElementException
+        driver.findElement(By.cssSelector("input#email111"));
+    }
+
+    @Test
+    public void TC_03_Only_Explicit_Found() {
+        driver.get("https://www.facebook.com/");
+
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#email")));
+    }
+    @Test
+    public void TC_04_Only_Explicit_Not_Found_Param_By() {
+        driver.get("https://www.facebook.com/");
+
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        // khi vào tìm element không tìm thấy
+        // polling mỗi nửa s tìm lại 1 lần
+        // khi hết timeout sẽ đánh fail testcase và throw exception: TimeoutException
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#email111")));
+    }
+    @Test
+    public void TC_05_Only_Explicit_Not_Found_Param_WebElement() {
+        driver.get("https://www.facebook.com/");
+
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        // khi vào tìm element không tìm thấy
+        // polling mỗi nửa s tìm lại 1 lần
+        // khi hết timeout sẽ đánh fail testcase và throw exception: NoSuchElementException
+        // -> k dùng implicit nên k find được element
+        explicitWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("input#email111"))));
+    }
+    @Test
+    public void TC_06_Mix_Implicit_Explicit() {
+        driver.get("https://www.facebook.com/");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        // Check case k tìm thấy element > thời gian testcase fail < 8
+        // implicit sẽ ưu tiên chạy trước, rồi Explicit sẽ chạy mà k cần chờ implicit chạy xong (Explicit sẽ chạy sau implicit nửa đến 1s)
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#email111")));
+    }
+
+    @AfterClass
+    public void afterClass() {driver.quit();}
+
+}
